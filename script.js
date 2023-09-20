8a1184ecefbdba7c54ad865eb861eb34d4ee0ab7
melinabed <melo36049@hotmail.com>
2023-09-20 10:51:13 -1000

    added logic to buttons

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+//Calls the createContainer function and gives logic to the select button letting users
+//determine the size of the squares
+
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function() {
     createContainer(16);
-    userSelection();
-    console.log("hi")
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener('click', function() {
+        let size = userSelection();
+        createContainer(size);
+    })
 })
 
+//Lets the columns and rows repeat to give board container dimensions of height and width.
+//Also grabs the container from the html.
 function createContainer(size) {
     let container = document.querySelector(".container");
 
@@ -14,20 +25,39 @@ function createContainer(size) {
 
     for(let i = 0; i < numDiv; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = "yellow";
+        div.addEventListener('mouseover', colorDiv);
         container.insertAdjacentElement('beforeend', div);
     }
 
 }
 
+//Prompts the user to select the number of squares and gives conditions if the input is invalid
 function userSelection() {
     let input = prompt("How many squares?");
     let message = document.querySelector("#message");
-        if (input == "") {
-            message.innerHTML = "Please provide a number";
+        if (input === "") {
+            message.innerHTML = "Please provide a number.";
         } else if (input < 0 || input > 100) {
             message.innerHTML = "Please provide a number between 1 and 100.";
         } else {
             message.innerHTML = "Play!";
+            return input;
         }
+}
+
+function colorDiv() {
+    if (color == "color") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetContainer() {
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
